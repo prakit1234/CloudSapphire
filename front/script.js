17e3f658b54e14c2c9b9b8550e7f80f1183c458e
Prakit <saratgogoi78@gmail.com>
2024-10-12 17:48:12 +0530

    Update script.js

--- front/script.js
+++ front/script.js
@@ -5,22 +5,31 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let particlesArray = [];
+const maxParticles = 200; // Increased number of particles
 
 class Particle {
-    constructor(x, y, size, speedX, speedY) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.size = size;
-        this.speedX = speedX;
-        this.speedY = speedY;
+        this.size = Math.random() * 5 + 2; // Random size between 2 and 7
+        this.speedX = Math.random() * 3 - 1.5; // Random speed in x direction
+        this.speedY = Math.random() * 3 - 1.5; // Random speed in y direction
     }
+
     update() {
         this.x += this.speedX;
         this.y += this.speedY;
-        if (this.size > 0.1) this.size -= 0.02;
+
+        // Bounce off the edges
+        if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+        if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+
+        // Fade out particles
+        if (this.size > 0.1) this.size -= 0.1;
     }
+
     draw() {
-        ctx.fillStyle = 'rgba(0, 217, 255, 0.7)';
+        ctx.fillStyle = `rgba(0, 217, 255, ${this.size / 7})`; // Fade based on size
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.closePath();
@@ -30,24 +39,23 @@ class Particle {
 
 function initParticles() {
     particlesArray = [];
-    for (let i = 0; i < 120; i++) {
-        let size = Math.random() * 4 + 1;
-        let x = Math.random() * canvas.width;
-        let y = Math.random() * canvas.height;
-        let speedX = Math.random() * 2 - 1;
-        let speedY = Math.random() * 2 - 1;
-        particlesArray.push(new Particle(x, y, size, speedX, speedY));
+    for (let i = 0; i < maxParticles; i++) {
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+        particlesArray.push(new Particle(x, y));
     }
 }
 
 function animateParticles() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < particlesArray.length; i++) {
-        particlesArray[i].update();
-        particlesArray[i].draw();
+    for (let particle of particlesArray) {
+        particle.update();
+        particle.draw();
     }
     requestAnimationFrame(animateParticles);
 }
 
+// Start the animation
 initParticles();
 animateParticles();
+
