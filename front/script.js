5e426179b8d0e9787472f70f504fa9ea4c96dd57
Prakit <saratgogoi78@gmail.com>
2024-10-12 17:19:13 +0530

    Update script.js

--- front/script.js
+++ front/script.js
@@ -17,10 +17,10 @@ class Particle {
     update() {
         this.x += this.speedX;
         this.y += this.speedY;
-        if (this.size > 0.2) this.size -= 0.1;
+        if (this.size > 0.1) this.size -= 0.02;
     }
     draw() {
-        ctx.fillStyle = 'rgba(255, 255, 255, 0.7)';
+        ctx.fillStyle = 'rgba(0, 217, 255, 0.7)';
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.closePath();
@@ -30,12 +30,12 @@ class Particle {
 
 function initParticles() {
     particlesArray = [];
-    for (let i = 0; i < 100; i++) {
-        let size = Math.random() * 5 + 1;
+    for (let i = 0; i < 120; i++) {
+        let size = Math.random() * 4 + 1;
         let x = Math.random() * canvas.width;
         let y = Math.random() * canvas.height;
-        let speedX = Math.random() * 3 - 1.5;
-        let speedY = Math.random() * 3 - 1.5;
+        let speedX = Math.random() * 2 - 1;
+        let speedY = Math.random() * 2 - 1;
         particlesArray.push(new Particle(x, y, size, speedX, speedY));
     }
 }
